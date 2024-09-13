b2e11f6b3fff5c2311012a6641c5f839b03ba5d9
byounggeol-cho <byounggeol.cho@nhn.com>
2024-09-13 17:50:38 +0900

    add support intellij 2024.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,30 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.15.0"
+    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.kotlin.jvm") version "1.9.0" // Kotlin 플러그인 추가
 }
 
 group = "com.bluewhale"
-version = "1.0.2"
+version = "1.0.4"
 
 repositories {
+    gradlePluginPortal()
     mavenCentral()
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2023.2.2")
-    type.set("IC") // Target IDE Platform
-
+    version.set("2024.2.1")
+    type.set("IC")
     plugins.set(listOf())
 }
 
 dependencies {
     implementation("org.yaml:snakeyaml:1.33")
     implementation("org.springframework.boot:spring-boot:2.7.5")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0") // Kotlin 표준 라이브러리 추가
 }
 
 tasks {
-    // Set the JVM compatibility versions
     withType<JavaCompile> {
         sourceCompatibility = "11"
         targetCompatibility = "11"
@@ -33,7 +32,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("221")
-        untilBuild.set("232.*")
+        untilBuild.set("252.*")
     }
 
     signPlugin {
@@ -45,4 +44,9 @@ tasks {
     publishPlugin {
         token.set(System.getenv("PUBLISH_TOKEN"))
     }
+
+    named<Test>("test") {
+        useJUnitPlatform()
+    }
 }
+
