8e2f1dd2e9b456870e54d6635226f0860caf0ceb
byounggeol-cho <byounggeol.cho@nhn.com>
2026-01-10 10:55:28 +0900

    1.0.7 upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,66 +1,68 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.17.3" // IntelliJ 플러그인 최신 버전
-    id("org.jetbrains.kotlin.jvm") version "1.9.0"
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.intellij.platform") version "2.3.0"
 }
 
 group = "com.bluewhale"
-version = "1.0.6"
+version = "1.0.7"
 
 repositories {
     mavenCentral()
-    gradlePluginPortal()
-}
-
-intellij {
-    version.set("2025.1.3")
-    type.set("IC") // IntelliJ Community Edition
-    plugins.set(listOf()) // 필요한 플러그인 ID가 있다면 여기에 추가
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
     implementation("org.yaml:snakeyaml:2.4")
     implementation("org.springframework.boot:spring-boot:3.4.3")
+    
+    intellijPlatform {
+        create("IC", "2025.1.3")
+    }
 }
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
-tasks {
-    patchPluginXml {
-        sinceBuild.set("251")
-        untilBuild.set("252.*")
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "251"
+            untilBuild = "253.*"
+        }
     }
-
-    buildSearchableOptions {
-        enabled = false
-    }
-
-    signPlugin {
+    
+    buildSearchableOptions = false
+    
+    signing {
         certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
         privateKey.set(System.getenv("PRIVATE_KEY"))
         password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
     }
-
-    publishPlugin {
+    
+    publishing {
         token.set(System.getenv("PUBLISH_TOKEN"))
     }
+}
 
+tasks {
     test {
         useJUnitPlatform()
     }
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = "17"
+    
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "21"
+        }
     }
-}
-
-tasks.withType<JavaCompile>().configureEach {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
+    
+    withType<JavaCompile>().configureEach {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
 }
